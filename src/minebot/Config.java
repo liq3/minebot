d1326e17ebcfd3955a0842245e1902456fdaccda
Conrad Morgan <conrad.morgan@gmail.com>
2011-07-07 13:52:15 -0700

    added Config thing

---
+++ src/minebot/Config.java
@@ -0,0 +1,34 @@
+package minebot;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Config {
+	public static String username = "";
+	public static String password = "";
+	
+	public static String host = "localhost";
+	public static int port = 25565;
+	
+	public static void Load(String filename) throws IOException {
+		BufferedReader file = new BufferedReader(new FileReader(filename));
+
+		String line;
+		while ((line = file.readLine()) != null) {
+			String[] pair = line.split("=");
+			String key = pair[0].trim().toLowerCase();
+			String value = pair[1].trim().toLowerCase();
+			
+			if (key.equals("username")) {
+				username = value;
+			} else if (key.equals("password")) {
+				password = value;
+			} else if (key.equals("host")) {
+				host = value;
+			} else if (key.equals("port")) {
+				port = Integer.parseInt(value);
+			}
+		}
+	}
+}
--- src/minebot/Minebot.java
+++ src/minebot/Minebot.java
@@ -10,27 +10,16 @@ public class Minebot
 	private static Session session;
 
 	public static void main(String args[]) throws Exception {
-		BufferedReader fileInput = null;
-		String username = null;
-		String password = null;
+		
 		try {
-			fileInput = new BufferedReader(new FileReader("login.txt"));
-			username = fileInput.readLine();
-			password = fileInput.readLine();
-		} finally {
-			if (fileInput != null) {
-				fileInput.close();
-			}
+			Config.Load("config.txt");
+		} catch (IOException e) {
+			System.out.println("config.txt missing");
 		}
 		
-		if (username == null || password == null) {
-			System.out.println("Invalid login.txt");
-			System.exit(0);
-		}
-		
-		session = new Session(username, password);
+		session = new Session(Config.username, Config.password);
 		session.login();
-		session.connect("59.167.126.221", 25565);
+		session.connect(Config.host, Config.port);
 		
 		player = new Player(session);
 		session.begin(player);
